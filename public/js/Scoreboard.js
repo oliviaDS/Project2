d729a2b39d8c647b6750d4e86f7357dcf583d237
Donovan Jong Wen <djw9413@hotmail.com>
2020-12-05 13:24:46 +0800

    "Changes"

--- public/js/Scoreboard.js
+++ public/js/Scoreboard.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
-var nameInput = $("")
+var nameInput = $("user-name");
+var userContainer = $(".user-container");
+var scoreList = $("");
+
+$(document).on("submit", "#user-form", handleUserFormSubmit);
+
+//Get list of Users
+getUsers();
+
+
+// Function for User form submission
+function handleUserFormSubmit(event){
+  event.preventDefault();
+  if(!nameInput.val().trim().trim()) {
+    return;
+  }
+  hailUser({
+    name: nameInput
+      .val()
+      .trim()
+  });
+}
+
+// Call user
+function hailUser(userData){
+  $.post("api/user", userData)
+    .then(getUsers);
+}
+
 
 
 };)
